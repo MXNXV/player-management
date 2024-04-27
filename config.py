9120890ba94b3432d8cad5dc6274f9308030f38e
Manav Mandal <63636038+MXNXV@users.noreply.github.com>
2024-04-27 00:31:16 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/stats'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@34.28.237.121/stats'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # other configuration variables
